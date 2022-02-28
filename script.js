b3143c52078ad41c44200e938c468c0039f621a4
Jacob Vernau <jacobvernau@gmail.com>
2022-02-28 14:35:43 -0700

    sandbox dynamic listing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const listItems = document.querySelector('li');
 
 function addListItem(item) {
   let el = document.createElement('li');
+  console.log(el);
   list.appendChild(el);
   el.textContent = `${item}`;
 }
@@ -11,3 +12,5 @@ addListItem('item 1');
 addListItem('item 2');
 
 listItems.style.listStyleType = 'none';
+
+listItems.style.display = 'inline-block';
